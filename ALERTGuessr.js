ef15faceb77a18d34858863f94c580d41c0037b5
KevCrawfish <78246147+KevCrawfish@users.noreply.github.com>
2025-07-09 22:28:13 -0700

    Updating GIT files to 1.0

--- ALERTGuessr.js
+++ ALERTGuessr.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         ALERTGuessr
 // @namespace    http://tampermonkey.net/
-// @version      2024-04-28
-// @description  Try to learn about the world!
+// @version      1.0
+// @description  Hover over the settings icon in the tagged anomalies section to find the start button (Reach out to me if you find any bugs)
 // @author       Kevin Crawford
 // @match        https://alertwest.live/secure/anomalies
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=alertwest.live
@@ -10,12 +10,15 @@
 // @grant        none
 // ==/UserScript==
 
+// Todo:
+// Online leaderboard?
+
 (function() {
     'use strict';
     function makeGuessr(){
         var me = this;
         this._awaitDeps = () => {
-            this._targetNode = $('div.feed-options');
+            this._targetNode = $('#fire-feed-wrap > div.feed-header > div.feed-options');
             if (this._targetNode.length > 0){
                 $('#next-button').html('Lock Oldest');
                 this._targetNode.append(
@@ -32,6 +35,7 @@
                             function off(){
                                 $('div#gameMap').remove();
                                 $('div#gameInput').remove();
+                                $('#guess-score').remove();
                                 $('#guessrBtnColor').attr('style', 'color: black')
                                 console.log('Closed Streetview');
                             }
@@ -48,6 +52,7 @@
             var loaded = 0;
             var latlon = {lat: 0, lng: 0}
             var panorama;
+            var best = 99999999;
 
             function haversine(lat1, lon1, lat2, lon2) {
                 // this function is super rad 😎
@@ -62,14 +67,17 @@
             };
 
             function keyupEvent(e){
-                if (e.key === 'Enter' || e.button == 1) {
+                if (e.key === 'Enter' || e.button == 1 || e.target.id == 'game-guess') {
                     var pattern = /((-?[0-9]{1,3}(?:.[0-9]{1,})?(?:[, ]{1,})?){2}|(-?[0-9]{1,3}(?:.[0-9]{1,})?(?:[° ]{1,})[0-9]{1,3}(?:.[0-9]{1,})?[' ]*(?:[0-9]{1,3}(?:.[0-9]{1,})?[&quot; ]{1,})?(?:[NSEWnsew])?[, ]{0,2}){2})/g;
                     if (pattern.test($('.coordinates-input').val())){
                         var dist = haversine(latlon.lat, latlon.lng, parseFloat($('.coordinates-input').val().split(',')[0]), parseFloat($('.coordinates-input').val().split(',')[1]));
-                        $('#guess-result').stop().show(0).html(`Your guess was ${Math.round(dist)} miles away!`);
+                        Math.round(dist) < best ? best = Math.round(dist) : null;
+                        $('#guess-result').stop().show(0).html(`Your guess was ${Math.round(dist)} mi away!`);
                         $('[id="pac-input"]').val(`${latlon.lat},${latlon.lng}`).trigger('submit');
+                        $('#guess-score').html(`Best Guess: ${best} mi`);
                         $('[id="pac-input"]').val('');
                         $('[id="game-next"]').html('Play again?');
+                        $('[id="game-guess"]').off();
                         $('#coordinates-input').off();
                     } else {
                         $('#guess-result').html('Please enter a valid set of coordinates.').show().fadeOut(3000);
@@ -92,9 +100,10 @@
                 if (status == 'OK' && loaded) {
                     latlon = { lat: data.location.latLng.lat(), lng: data.location.latLng.lng() };
                     $('#guess-result').html('');
-                    $('#game-next').html('Next');
+                    $('#game-next').html('Skip').prop('disabled', false);
                     $('#coordinates-input').val('');
                     panorama.setPosition(latlon);
+                    $('[id="game-guess"]').off().on('click', (e) => keyupEvent(e));
                     $('#coordinates-input').off().on('keyup mousedown', (e) => keyupEvent(e));
                 } else if (status == 'OK' && !loaded) {
                     $('div#fire-feed').prepend([
@@ -105,14 +114,20 @@
                             $('<button>').attr({
                                 'class': 'btn-primary',
                                 'id': 'game-next',
-                                'style': 'border-radius: 3px; border: 0; '
-                            }).html('Next').on('click', () => {
+                                'style': 'margin-right: 5px; border-radius: 3px; border: 0; '
+                            }).html('Skip').on('click', () => {
+                                $('#game-next').html('Loading...').prop('disabled', true);
                                 TryRandomLocation(HandleCallback);
                             }),
+                            $('<button>').attr({
+                                'class': 'btn-primary',
+                                'id': 'game-guess',
+                                'style': 'border-radius: 3px; border: 0; '
+                            }).html('Guess').on('click', (e) => keyupEvent(e)),
                             $('<input>').attr({
                                 'class': 'coordinates-input',
                                 'id': 'coordinates-input',
-                                'placeholder': 'Input guess coordinates',
+                                'placeholder': 'Enter guess coordinates',
                                 'style': 'margin-left: 10px; font: 400 11px Roboto, Arial, sans-serif; width: 150.5px;'
                             }).on('keyup mousedown', (e) => keyupEvent(e)),
                             $('<p>').attr({
@@ -123,7 +138,11 @@
                         $('<div>').attr({
                             'style': 'width: 602px; height: 473px; margin-bottom: 10px',
                             'id': 'gameMap'
-                        })
+                        }),
+                        $('<p>').attr({
+                            'style': 'height: 24px; color: yellow; margin-left: 10px',
+                            'id': 'guess-score'
+                        }).html('Best Guess:')
                     ])
                     $('#guessrBtnColor').attr('style', 'color: green');
                     $('#guessrBtn').prop('disabled', false);
